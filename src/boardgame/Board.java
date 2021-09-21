4f8f452478a3465d2978b21af7be49ef83c3b712
Marcio Nunes <marcio-junior@hotmail.com>
2021-09-21 10:04:22 -0300

    Moving Pieces

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -46,6 +46,22 @@ public class Board {
 		 e foi definida como protected*/
 	}
 	
+	public Piece removePiece(Position position) {
+		if(!positionExists(position)) {
+			throw new BoardException("Position not on the board");
+		}
+		//Se não tem peça na posição
+		if(piece(position) == null) {
+			return null;
+		}
+		//Retira peça do tabuleiro
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+		
+	}
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
