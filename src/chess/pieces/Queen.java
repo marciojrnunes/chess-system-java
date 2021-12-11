7b1b3a071026bc35d7ad8956def169704727c2de
Marcio Nunes <marcio-junior@hotmail.com>
2021-12-11 07:22:31 -0300

    Queen, Pawn, Bishop and Knight

---
+++ src/chess/pieces/Queen.java
@@ -0,0 +1,126 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece{
+
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		//Mostra a inicial da peça no tabuleiro
+		return "Q";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {  //Movimentos possiveis da torre
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		//Posição inicial
+		Position p = new Position(0, 0);
+		
+		//Verifica as posições acima
+		p.setValues(position.getRow() - 1,	position.getColumn());
+		//Enquanto não tiver um peça na posicao acima
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()-1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições a esquerda
+		p.setValues(position.getRow(),	position.getColumn()-1);
+		//Enquanto não tiver um peça na posicao a esquerda
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()-1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições a direita
+		p.setValues(position.getRow(),	position.getColumn()+1);
+		//Enquanto não tiver um peça na posicao a direita
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn()+1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições abaixo
+		p.setValues(position.getRow() + 1,	position.getColumn());
+		//Enquanto não tiver um peça na posicao abaixo
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow()+1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições NW
+		p.setValues(position.getRow() - 1,	position.getColumn()-1);
+		//Enquanto não tiver um peça na posicao acima
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow()-1, p.getColumn()-1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições NE
+		p.setValues(position.getRow()-1,	position.getColumn()+1);
+		//Enquanto não tiver um peça na posicao a esquerda
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow()-1, p.getColumn()+1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições SE
+		p.setValues(position.getRow() + 1,	position.getColumn()+1);
+		//Enquanto não tiver um peça na posicao a direita
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow()+1, p.getColumn()+1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Verifica as posições SW
+		p.setValues(position.getRow() + 1,	position.getColumn()-1);
+		//Enquanto não tiver um peça na posicao abaixo
+		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow()+1, p.getColumn()-1);
+		}
+		//Verifica se na próxima posição tem peça adversária
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return mat;
+	}
+
+}
